b14548b06eb3d309b030e6a54f3654b8b413fea4
Primolo <primo.locatelli@gmail.com>
2025-10-28 13:21:59 +0100

    Update wsgi.py

--- ULMG70_Manager/wsgi.py
+++ ULMG70_Manager/wsgi.py
@@ -1,1 +1,8 @@
+import os
 
+from django.core.wsgi import get_wsgi_application
+
+# Assurez-vous que le nom du module de configuration est bien réglé sur 'ULMG70_Manager.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ULMG70_Manager.settings')
+
+application = get_wsgi_application()

bd3fef1487b8393a972bb5b38de025042bf98995
Primolo <primo.locatelli@gmail.com>
2025-10-28 08:44:43 +0100

    Update settings.py

--- ULMG70_Manager/settings.py
+++ ULMG70_Manager/settings.py
@@ -1,1 +1,12 @@
+# ULMG70_Manager/settings.py (extrait)
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # --- AJOUTER CETTE LIGNE ---
+    'copro.apps.CoproConfig', 
+]

82aa2fee4d9a424f0ecdebc96753af50be052c2c
Primolo <primo.locatelli@gmail.com>
2025-10-28 08:44:29 +0100

    Update urls.py

--- ULMG70_Manager/urls.py
+++ ULMG70_Manager/urls.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include 
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Lie les URLs de l'application 'copro' à la racine du site
+    path('', include('copro.urls')), 
+]

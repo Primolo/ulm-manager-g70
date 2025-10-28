07b53ab5fd5cea1a299f483f75e926c59e02d0bd
Primolo <primo.locatelli@gmail.com>
2025-10-28 08:43:58 +0100

    Update urls.py

--- copro/urls.py
+++ copro/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from .views import ReservationListView
 
+urlpatterns = [
+    # Adresse de base : monapp.com/
+    path('', ReservationListView.as_view(), name='reservation_list'),
+]

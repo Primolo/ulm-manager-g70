5c22ece0963d2346c91c578dfb46d18162f46cf3
Primolo <primo.locatelli@gmail.com>
2025-10-28 15:49:19 +0100

    Update views.py

--- copro/views.py
+++ copro/views.py
@@ -1,18 +1,18 @@
 from django.views.generic import ListView
 
-# --- Assure-toi que les imports des modèles et des outils sont en haut ---
+# Imports nécessaires en tête de fichier pour la pérennité
 from .models import Reservation
 from django.utils import timezone 
 
-# -------------------------------------------------------------------------
-
 # Affiche la liste des réservations futures
 class ReservationListView(ListView):
-    model = Reservation
+    # Lien vers le fichier HTML
     template_name = 'copro/reservation_list.html'
+    
+    # Nom de la variable utilisée dans le template HTML
     context_object_name = 'reservations'
 
     def get_queryset(self):
-        # Cette ligne est maintenant propre
-        # Elle demande toutes les réservations dont la date de fin est supérieure ou égale à maintenant
+        # Récupère le queryset. On ne définit pas le modèle ici pour éviter une erreur
+        # lors de l'initialisation. On filtre directement les réservations futures.
         return Reservation.objects.filter(date_fin__gte=timezone.now()).order_by('date_debut')

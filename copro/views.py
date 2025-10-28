3745e86442bdf6a4c3a27e81e866532e1e93c12e
Primolo <primo.locatelli@gmail.com>
2025-10-28 15:53:30 +0100

    Update views.py

--- copro/views.py
+++ copro/views.py
@@ -1,18 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import TemplateView # Change de ListView à TemplateView
 
-# Imports nécessaires en tête de fichier pour la pérennité
-from .models import Reservation
-from django.utils import timezone 
+# --- Les autres imports ne sont plus nécessaires pour l'instant ---
+# from .models import Reservation
+# from django.utils import timezone 
 
-# Affiche la liste des réservations futures
-class ReservationListView(ListView):
-    # Lien vers le fichier HTML
+# Affiche la liste des réservations futures (temporairement en TemplateView)
+class ReservationListView(TemplateView):
+    # Lien vers le fichier HTML. AUCUN code de base de données n'est exécuté.
     template_name = 'copro/reservation_list.html'
     
-    # Nom de la variable utilisée dans le template HTML
-    context_object_name = 'reservations'
-
-    def get_queryset(self):
-        # Récupère le queryset. On ne définit pas le modèle ici pour éviter une erreur
-        # lors de l'initialisation. On filtre directement les réservations futures.
-        return Reservation.objects.filter(date_fin__gte=timezone.now()).order_by('date_debut')
+    # Nous n'avons pas besoin de get_queryset() pour l'instant
+    
+# --- Fin de la vue ---

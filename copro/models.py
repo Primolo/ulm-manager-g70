23a08f8675fe9bfc49c3b5e52c940527e1cf2f0b
Primolo <primo.locatelli@gmail.com>
2025-10-29 08:01:26 +0100

    Update models.py

--- copro/models.py
+++ copro/models.py
@@ -1,6 +1,8 @@
 
 from django.db import models
-from django.contrib.auth.models import CopropietaireProfile
+from django.contrib.auth.models import User # Pour lier à l'utilisateur Django
+# L'importation de CopropietaireProfile ici n'est PAS nécessaire
+# L'importation de LogEntry non plus, sauf si un autre fichier les importait
 
 # --- Modèle 1 : Profil des Copropriétaires ---
 class CopropietaireProfile(models.Model):

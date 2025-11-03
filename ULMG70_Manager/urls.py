4c3756a76252829489a33d1c100bf12e1872a10d
Primolo <primo.locatelli@gmail.com>
2025-11-03 15:52:49 +0100

    Update urls.py

--- ULMG70_Manager/urls.py
+++ ULMG70_Manager/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Lie les URLs de l'application 'copro' à la racine du site
-    path('', include('copro.urls')), 
+    # 1. URLS DU THÈME JET (DOIT ÊTRE EN PREMIER)
+    path('jet/', include('jet.urls', 'jet')),
+    # 2. URLS DU DASHBOARD JET
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
+    # ----------------------------------------
+    path('admin/', admin.site.urls),
+    # Lie les URLs de l'application 'copro' à la racine du site
+    path('', include('copro.urls')), 
 ]

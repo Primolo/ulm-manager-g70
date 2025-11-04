bf57c719f80984de4ca77e1bfda6959e1a523461
Primolo <primo.locatelli@gmail.com>
2025-11-04 09:06:01 +0100

    Update views.py

--- copro/views.py
+++ copro/views.py
@@ -3,37 +3,39 @@ from django.urls import reverse_lazy
 from django.utils import timezone
 from django.shortcuts import render 
 
-# 1. IMPORTER LES MODÈLES EN PREMIER
+# --- NOUVEAUX IMPORTS POUR LE FLUX JSON ---
+from django.http import JsonResponse
+from django.views import View 
+from datetime import datetime, date
+# ------------------------------------------
+
+# Imports de l'Application (Vérifié et Stable)
 from .models import Reservation, LogEntry 
-# 2. IMPORTER LES FORMULAIRES EN DERNIER
 from .forms import ReservationForm, LogEntryForm 
 
-
 # --- Vues du Dashboard ---
-# ... (Les classes de vues sont inchangées et utilisent les objets modèles et formulaires)
 
-class ReservationListView(ListView):
-    model = Reservation
-    template_name = 'copro/reservation_list.html'
-    context_object_name = 'object_list'
-    def get_queryset(self):
-        return Reservation.objects.filter(date_fin__gte=timezone.now()).order_by('date_debut')
+# Vue 1 : Affiche la Liste des Réservations (Dashboard Page d'Accueil)
+# ... (Les classes ReservationListView, LogEntryCreateView, ReservationCreateView, LogbookListView sont ici) ...
 
-class LogEntryCreateView(CreateView):
-    model = LogEntry
-    form_class = LogEntryForm
-    template_name = 'copro/logentry_form.html'
-    success_url = reverse_lazy('reservation_list')
-
-class ReservationCreateView(CreateView):
-    model = Reservation
-    form_class = ReservationForm
-    template_name = 'copro/reservation_form.html'
-    success_url = reverse_lazy('reservation_list')
-
-class LogbookListView(ListView):
-    model = LogEntry
-    template_name = 'copro/logbook_list.html'
-    context_object_name = 'logbook_entries' 
-    def get_queryset(self):
-        return LogEntry.objects.all().select_related('pilote').order_by('-date_vol')
+# Vue 5 : Fournit les données des réservations au format JSON pour FullCalendar
+class ReservationJsonFeed(View):
+    def get(self, request, *args, **kwargs):
+        # Récupère toutes les réservations
+        reservations = Reservation.objects.all()
+        
+        events = []
+        for reservation in reservations:
+            # FullCalendar utilise 'title', 'start', et 'end'
+            events.append({
+                'title': f"Réservé par {reservation.coproprietaire.user.username}",
+                # Utilise isoformat() pour le format de date et heure universel requis par JS
+                'start': reservation.date_debut.isoformat(), 
+                'end': reservation.date_fin.isoformat(),
+                # URL de modification (pointe vers le formulaire d'ajout pour l'instant)
+                'url': reverse_lazy('reservation_add'), 
+                'allDay': False
+            })
+        
+        # 'safe=False' est nécessaire car la liste JSON n'est pas un dictionnaire de haut niveau
+        return JsonResponse(events, safe=False)

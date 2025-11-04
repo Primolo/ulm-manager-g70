990ab4bea8062c0222531b12ee07fc0c380a6679
Primolo <primo.locatelli@gmail.com>
2025-11-04 06:52:04 +0100

    Update views.py

--- copro/views.py
+++ copro/views.py
@@ -1,24 +1,36 @@
-from django.views.generic import ListView, CreateView
-from django.urls import reverse_lazy 
-from .models import Reservation, LogEntry # Assure-toi que LogEntry est importé
-from .forms import LogEntryForm # Assure-toi que LogEntryForm est importé
+from django.views.generic import ListView, CreateView, TemplateView # Ajout de TemplateView
+from django.urls import reverse_lazy
+from django.utils import timezone # Nécessaire pour la fonction get_queryset()
 
-# La vue du Dashboard / Liste des Réservations
+# --- Imports des modèles et formulaires (Utilisation de l'importation absolue pour stabiliser) ---
+from copro.models import Reservation, LogEntry 
+from copro.forms import LogEntryForm, ReservationForm 
+# --------------------------------------------------------------------------------------------------
+
+# --- Vues du Dashboard ---
+
+# Vue 1 : Affiche la Liste des Réservations (Dashboard)
 class ReservationListView(ListView):
+    # model = Reservation # Retirer model ici est souvent plus stable
     template_name = 'copro/reservation_list.html'
-    context_object_name = 'object_list' # Renommer pour être standard
+    context_object_name = 'object_list'
 
     def get_queryset(self):
-        # Récupère et filtre les réservations futures
+        # Récupère et filtre les réservations futures (nécessite timezone)
         return Reservation.objects.filter(date_fin__gte=timezone.now()).order_by('date_debut')
 
-# Vue pour le Journal de Bord (sera complétée plus tard)
-
-# Vue pour l'ajout d'une entrée au Journal de Bord
+# Vue 2 : Ajout d'une Entrée au Journal de Bord (Carré "Enregistrer un Vol")
 class LogEntryCreateView(CreateView):
     model = LogEntry
     form_class = LogEntryForm
     template_name = 'copro/logentry_form.html'
+    success_url = reverse_lazy('reservation_list')
     
-    # Redirige vers la page d'accueil après le succès
+    # NOTE : La logique pour attribuer le pilote à l'utilisateur connecté peut être ajoutée ici plus tard.
+
+# Vue 3 : Ajout d'une Réservation (Carré "Gérer les Réservations")
+class ReservationCreateView(CreateView):
+    model = Reservation
+    form_class = ReservationForm
+    template_name = 'copro/reservation_form.html'
     success_url = reverse_lazy('reservation_list')
